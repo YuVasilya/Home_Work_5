ca1fc6183eea0a43ee8c6355acb4592dec7d1fd7
Vasilya <lady_drakula_94@mail.ru>
2022-10-06 22:27:41 +0500

    ignore внесен

---
+++ Ex04_Multiply/Program.cs
@@ -0,0 +1,17 @@
+﻿// для любознательных: Написать программу премножения любых целых чисел
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+int a = new Random().Next(1, 10000);
+int b = new Random().Next(1, 200000);
+
+int Multiply(int num1, int num2)
+{
+int answer = a*b;
+return answer;
+}
+
+Console.WriteLine($"First number = {a}");
+Console.WriteLine($"Second number = {b}");
+Console.Write("Numbers multiply = ");
+Console.WriteLine(Multiply(a,b));
