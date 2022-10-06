56af77d21ab7474362d398df9ebab3c9f76f1343
Vasilya <lady_drakula_94@mail.ru>
2022-10-06 14:26:35 +0500

    Ex01 работает, надо поработать с оформлением массива

---
+++ Ex01_Even_nimber_count_in_array/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив, заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет колличество чётных числе в массиве.
+Console.Clear();
+
+int [] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void Fill(int[] array, int minValue, int maxValue)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+}
+
+void Print(int [] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i]+",");
+    }
+    Console.WriteLine();
+}
+
+int Count_Even_Num(int[] array)
+{
+    int size = array.Length;
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Hello, Teacher");
+
+int numlen = new Random().Next(1, 101);
+Console.WriteLine($"array.Length = {numlen}");
+
+int [] numbers = CreateArray(numlen);
+
+Fill(numbers, 100, 1000);
+
+Print(numbers);
+
+Console.WriteLine($"Count even numbers = {Count_Even_Num(numbers)}");
+
